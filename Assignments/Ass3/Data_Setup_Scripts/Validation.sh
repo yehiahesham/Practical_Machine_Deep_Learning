29d320b0429b1c7177078f20cb47925bec696bf1
Yehia Said <yehiahesham@aucegypt.edu>
2017-04-11 13:28:55 +0200

    new scripts

--- Assignments/Ass3/Data_Setup_Scripts/Validation.sh
+++ Assignments/Ass3/Data_Setup_Scripts/Validation.sh
@@ -1,6 +1,9 @@
-for d in /home/yehiahesham/Desktop/vi/ML/Ass3_data/train/* ; do
+mkdir /home/yehiahesham/Desktop/ML/Ass3_data/validate/
+cd /home/yehiahesham/Desktop/ML/Ass3_data/validate/
+for d in /home/yehiahesham/Desktop/ML/Ass3_data/train/* ; do
+	mkdir ${d#/home/yehiahesham/Desktop/ML/Ass3_data/train/}
     for (( i=0; i <= 49; i++ ))
     do
-        mv $d/*_$i.JPEG /home/yehiahesham/Desktop/vi/ML/Ass3_data/validate/
+        mv $d/*_$i.JPEG ${d#/home/yehiahesham/Desktop/ML/Ass3_data/train/}
     done
 done
